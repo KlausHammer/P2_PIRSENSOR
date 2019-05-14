fb333334ad5270a3353b620f4bac1e3cfd5e0e20
NielsDyrberg <ncpd@live.dk>
2019-05-14 22:08:50 +0200

    Stating up

--- Kode/EITB225_PIRSensor_Arduino.cpp
+++ Kode/EITB225_PIRSensor_Arduino.cpp
@@ -1,10 +1,51 @@
 #include <Arduino.h>
+#include "EITB225_PIRSensor_Arduino.h"
+
+uint8_t sen1Pin = 2;
+uint8_t sen2Pin = 3;
+uint8_t sen3Pin = 21;
+
+float encodeRatio = 360/374;
+
+int pirPos[] = {
+		-100*encodeRatio,
+		-50*encodeRatio,
+		0,
+		50*encodeRatio,
+		100*encodeRatio
+};
+
+
+void sen1(){
+
+}
+
+void sen2(){
+
+}
+
+void sen3(){
+
+}
+
+
+
+
 
 void setup() {
+
+	attachInterrupt(digitalPinToInterrupt(sen1Pin), sen1, RISING);
+	attachInterrupt(digitalPinToInterrupt(sen2Pin), sen2, RISING);
+	attachInterrupt(digitalPinToInterrupt(sen3Pin), sen3, RISING);
 
 }
 
 void loop() {
+	int waiter = 0;
+
+	while(waiter == 0);
+
+	move
 
 }
 
---
+++ Kode/EITB225_PIRSensor_Arduino.h
@@ -0,0 +1,15 @@
+/*
+ * EITB225_PIRSensor_Arduino.h
+ *
+ *  Created on: 14. maj 2019
+ *      Author: NielsDyrberg
+ */
+
+#ifndef EITB225_PIRSENSOR_ARDUINO_H_
+#define EITB225_PIRSENSOR_ARDUINO_H_
+
+
+
+
+
+#endif /* EITB225_PIRSENSOR_ARDUINO_H_ */
