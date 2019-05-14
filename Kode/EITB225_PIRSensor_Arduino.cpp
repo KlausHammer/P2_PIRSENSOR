2a1e15582b3a1bc68a553a809418a03e8e0955a6
NielsDyrberg <ncpd@live.dk>
2019-05-14 22:36:33 +0200

    Fuckery

--- Kode/EITB225_PIRSensor_Arduino.cpp
+++ Kode/EITB225_PIRSensor_Arduino.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include "EITB225_PIRSensor_Arduino.h"
+//#include "src/moveFuncs.cpp"
+//#include "src/kali.cpp"
 
-uint8_t sen1Pin = 2;
-uint8_t sen2Pin = 3;
-uint8_t sen3Pin = 21;
+const uint8_t sen1Pin = 2;
+const uint8_t sen2Pin = 3;
+const uint8_t sen3Pin = 21;
+
+int waiter = 0;
 
 float encodeRatio = 360/374;
 
@@ -18,6 +22,8 @@ int pirPos[] = {
 
 void sen1(){
 
+
+	waiter = 1;
 }
 
 void sen2(){
@@ -28,24 +34,22 @@ void sen3(){
 
 }
 
-
-
-
-
 void setup() {
-
+	Serial.begin(9600);
 	attachInterrupt(digitalPinToInterrupt(sen1Pin), sen1, RISING);
 	attachInterrupt(digitalPinToInterrupt(sen2Pin), sen2, RISING);
 	attachInterrupt(digitalPinToInterrupt(sen3Pin), sen3, RISING);
 
+	//kalibrering();
+
 }
 
 void loop() {
-	int waiter = 0;
+	int test2;
 
 	while(waiter == 0);
 
-	move
+	//move(test2);
 
 }
 
