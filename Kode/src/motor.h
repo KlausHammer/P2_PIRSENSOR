37e4ceeb7b05a348065fb2323a7116af0f3413a0
NielsDyrberg <ncpd@live.dk>
2019-05-20 15:11:40 +0200

    Class PirS Works

--- Kode/src/motor.h
+++ Kode/src/motor.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * motor.h
  *
@@ -33,38 +33,3 @@ extern void move(int taks);
 
 
 #endif /* SRC_MOTOR_H_ */
-=======
-/*
- * motor.h
- *
- *  Created on: 14. maj 2019
- *      Author: NielsDyrberg
- */
-
-#ifndef SRC_MOTOR_H_
-#define SRC_MOTOR_H_
-
-#include <Arduino.h>
-#include "../EITB225_PIRSensor_Arduino.h"
-#include "../lib/encoder/Encoder.h"
-
-const uint8_t encA = 18;
-const uint8_t encB = 19;
-const uint8_t dirPin = 7;
-
-const uint8_t motorPWM = 44;
-const int maxSpeed = 100;
-const int minSpeed = 20;
-
-const float Kp = 1.00;
-const float Ki = 0.001;
-const float Kd = 1.00;
-
-extern void kalibrering();
-extern void resetEncoder();
-extern int pid(int error);
-extern void move(int taks);
-
-
-#endif /* SRC_MOTOR_H_ */
->>>>>>> refs/remotes/origin/master
